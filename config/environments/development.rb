ce7c0828487e6dbda54e93173a83fd6182610da1
Tamashua <web@tamashua.com>
2013-03-23 17:50:48 -0400

    Adding Images and Sizing Specs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,4 +37,7 @@ Omrails::Application.configure do
 
 config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 #In production host should be set to application
+
+Paperclip.options[:command_path] = "usr/local/bin"
+
 end
